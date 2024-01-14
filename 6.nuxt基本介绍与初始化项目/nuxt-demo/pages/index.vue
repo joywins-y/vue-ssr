1744bc6799a386d7d961a56f59be1e2ca677e801
joy <1519105541@qq.com>
2024-01-14 18:29:18 +0800

    feat: 6. nuxt 基本介绍与初始化项目

---
+++ 6.nuxt基本介绍与初始化项目/nuxt-demo/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <Tutorial />
+        <Demo />
+        <router-link to="about">About</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "IndexPage",
+};
+</script>
